a90885944934d3e43d5fd8b657fdd4bc24de83f0
Jason Bolonski <jason.bolonski@gmail.com>
2017-01-08 14:05:24 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # temperature-publish
+
+Arduino reading temperature from sensor and feeds data via serial back to python program for publishing to mqtt
+
+Send a "T" to the arduino it will return json data with the temperature setting.
---
+++ temp_mqtt_publish.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import yaml
+import serial
+from time import sleep
+from datetime import datetime
+import paho.mqtt.client as mqtt
+import json
+
+print "Starting Temperature Monitor"
+
+print "Loading Configuration"
+with open("configuration.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+
+print "Connecting to MQTT @ " + cfg['mqtt']['server']
+
+mqttc=mqtt.Client()
+mqttc.username_pw_set(username=cfg['mqtt']['username'], password=cfg['mqtt']['password'])
+mqttc.connect(cfg['mqtt']['server'],cfg['mqtt']['port'],60)
+mqttc.loop_start()
+
+print "Connecting to Arduino"
+
+arduino=serial.Serial(cfg['arduino']['device'],cfg['arduino']['baud'], timeout=cfg['arduino']['timeout'])
+with arduino:
+    arduino.setDTR(False)
+    sleep(1)
+    arduino.flushInput()
+    arduino.flushOutput()
+    arduino.setDTR(True)
+    
+    arduino.write(str('H'))
+    sleep(1)
+    connect=arduino.readline()
+    print connect
+
+    while True:
+        arduino.write(str('T'))
+        sleep(1)
+        result=arduino.readline()
+        data=json.loads(result)
+        tempC = float(data["Temperature"])
+        tempF = (tempC * 1.8)+32
+        tempData = "{0:.1f}".format(tempF)
+        timeData = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        try:
+	  mqttc.publish("sensors/arduino1/temp",tempData,2)
+        except:
+	  print 'error publishing'
+
+	logData = timeData + ", " + tempData + " F"
+	print logData
+
+        sleep( cfg['intervals']['temperature'] )
+
+
+mqttc.loop_stop()
+mqttc.disconnect()
+
